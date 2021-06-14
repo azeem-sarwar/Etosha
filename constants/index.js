6ad13d25323e60737b38415eeb5b9946504a5380
Azeem Sarwar <as1265513@gmail.com>
2021-06-14 14:30:17 +0500

    mEUNAL SETUP

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import icons from "./icons";
+import images from "./images";
+import { COLORS, SIZES, FONTS,fontsFamily } from "./theme";
+import {GOOGLE_API_KEY_andriod,GOOGLE_API_KEY_ios }from "./maps"
+
+export { icons, images, COLORS, SIZES, FONTS, GOOGLE_API_KEY_andriod,GOOGLE_API_KEY_ios,fontsFamily };
---
+++ constants/maps.js
@@ -0,0 +1,5 @@
+const GOOGLE_API_KEY_andriod = "AIzaSyBemOjy8lTJVj5xyukW6e7eCTl3iTu-q00"
+const GOOGLE_API_KEY_ios = "AIzaSyBemOjy8lTJVj5xyukW6e7eCTl3iTu-q00"
+
+
+export {GOOGLE_API_KEY_andriod,GOOGLE_API_KEY_ios}
